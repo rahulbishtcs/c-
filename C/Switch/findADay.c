db07a199471410bed57c89c1535c40d1596c61ff
Rahul Bisht <98369722+rahulbishtcs@users.noreply.github.com>
2022-04-05 06:30:04 +0530

    Create findADay.c

---
+++ C/Switch/findADay.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    
+    int x;
+    float f,c;
+    printf("Enter the no. between 1 to 7\n");
+    scanf("%d",&x);
+    switch(x){
+        default:printf("Wrong Input");
+                break;
+        case 1:printf("Monday");
+                break;
+        case 2:printf("Monday");
+                break;
+        case 3:printf("Monday");
+                break;
+        case 4:printf("Monday");
+                break;
+        case 5:printf("Monday");
+                break;
+        case 6:printf("Monday");
+                break;
+        case 7:printf("Monday");
+                break;
+    }
+    return(0);
+}
