00c1f5036710b7059289d9fe821e328b7ce2e1fc
Rahul Bisht <98369722+rahulbishtcs@users.noreply.github.com>
2022-04-11 17:40:27 +0530

    Create DataType.js

---
+++ JavaScript/DataType.js
@@ -0,0 +1,14 @@
+//Data Type
+//string, Number, boolean, undefined, null, bigint, symbol
+let myName = "Rahul Bisht";
+console.log(typeof(myName));
+
+//convert number to string
+let age = 24;
+age = age + ""
+console.log(typeof(age))
+
+//convert string to number
+let rollNo = +"34"  //put + sign before string
+console.log(typeof(rollNo))
+
