ffe143310d1516dccfa1d2f0e33dc9bb78c4618b
Rahul Bisht <98369722+rahulbishtcs@users.noreply.github.com>
2022-04-11 18:21:51 +0530

    Create switch.js

---
+++ JavaScript/switch.js
@@ -0,0 +1,10 @@
+let charater = prompt("Enter a character")
+switch(charater){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':console.log("Vowel")
+            break
+    default: console.log("Consonent")
+}
